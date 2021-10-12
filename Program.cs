350c86f3cde254a01b80f345b8934abad3fd8d97
Peter Blanchfield <zygomy@gmail.com>
2021-10-12 11:28:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//Want to open a file so
+using System.IO;
+
+namespace SwitchesAndUnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a Text Reader
+            TextReader tr = new StreamReader("D:/CourseNotes/GetStarted.csv");
+            //SOmething to read it to
+            string inputLine;
+            while ((inputLine = tr.ReadLine()) != null)
+            { 
+                //And write the contents to the screen
+                Console.WriteLine(inputLine); 
+            }
+        }
+    }
+}
