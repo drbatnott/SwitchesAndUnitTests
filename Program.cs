e16cf1c8d382b64886484488150d2c3ed37a3d46
Peter Blanchfield <zygomy@gmail.com>
2021-10-12 12:17:28 +0100

    OK Several ways of dealing with the input. The final one is shortest but less versatile. You will need to edit the code to get back to the switch

--- Program.cs
+++ Program.cs
@@ -12,10 +12,15 @@ namespace SwitchesAndUnitTests
             TextReader tr = new StreamReader("D:/CourseNotes/GetStarted.csv");
             //SOmething to read it to
             string inputLine;
+            inputLine = tr.ReadLine();
+            string[] outputs = {"error", " is a type of penguin!",
+                " is a CD"," is a game!", " is unknown!"
+            };         
             while ((inputLine = tr.ReadLine()) != null)
-            { 
-                //And write the contents to the screen
-                Console.WriteLine(inputLine); 
+            {
+                string[] parts = inputLine.Split(',');
+                int p0 = Int16.Parse(parts[0]);
+                Console.WriteLine(parts[1] + outputs[p0]);
             }
         }
     }
